d879ac61f0c9f426f61997270c351e94910cbcaf
Andres Correa <andrescorreac@gmail.com>
2023-09-07 16:13:55 -0500

    Agrega ui sin css ejercicio para practicar la manipulación del DOM desde js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 let playerCounter = 0
 let computerCounter = 0
 
+const rock = document.querySelector(".rock")
+const paper = document.querySelector(".paper")
+const scissors = document.querySelector(".scissors")
+const results = document.querySelector(".results")
+const parcialPlayerScore = document.querySelector(".parcial-player-score")
+const parcialComputerScore = document.querySelector(".parcial-computer-score")
+const lastGameResult = document.querySelector(".last-game-result")
+
+function verifyResult(){
+    if (playerCounter === computerCounter) {
+        results.textContent = "empate"  
+    } else if (playerCounter > computerCounter) {
+        results.textContent = "ganaste" 
+    } else if (playerCounter < computerCounter) {
+        results.textContent = "perdiste" 
+    }
+}
+
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -9,49 +27,64 @@ function getComputerChoice(){
     number = randomNumber(1,3)
     switch (number) {
         case 1:
-            choise = "Rock"
-            break;
+            choise = "rock"
+            return choise.toLowerCase()
         case 2:
-            choise = "Paper"
-            break;
+            choise = "paper"
+            return choise.toLowerCase()
         case 3:
-                choise = "Scissors"
-            break;
+                choise = "scissors"
+            return choise.toLowerCase()
     }
-    return choise.toLowerCase()
 }
 
+
+alert("Let's play best of 5")
+
+rock.addEventListener("click", () => {
+    if (playerCounter == 5 || computerCounter == 5) {verifyResult()} 
+    else if (playerCounter < 5 || computerCounter < 5)  {playRound(getComputerChoice(), "rock")}
+})
+paper.addEventListener("click", () => {
+    if (playerCounter == 5 || computerCounter == 5) {verifyResult()} 
+    else if (playerCounter < 5 || computerCounter < 5) {playRound(getComputerChoice(), "paper")}
+})
+scissors.addEventListener("click", () => {
+    if (playerCounter < 5 || computerCounter < 5) {playRound(getComputerChoice(), "scissors")}
+})
+
+
+
+parcialPlayerScore.textContent = playerCounter
+parcialComputerScore.textContent = computerCounter
 function playRound(computerSelection, playerSelection) {
     if (playerSelection === computerSelection) {
-        return "Draw"
+        lastGameResult.textContent = "Empate"
+        if (playerCounter == 5 || computerCounter == 5) {verifyResult()} 
     } else if(playerSelection === "rock" & computerSelection === "scissors") {
-        return ++playerCounter 
+        playerCounter= ++playerCounter 
+        parcialPlayerScore.textContent = playerCounter
+        lastGameResult.textContent = "Victoria"
+        if (playerCounter == 5 || computerCounter == 5) {verifyResult()} 
     } else if(playerSelection === "paper" & computerSelection === "rock") {
-        return ++playerCounter 
+        playerCounter= ++playerCounter 
+        parcialPlayerScore.textContent = playerCounter
+        lastGameResult.textContent = "Victoria"
+        if (playerCounter == 5 || computerCounter == 5) {verifyResult()} 
     } else if(playerSelection === "scissors" & computerSelection === "paper") {
-        return ++playerCounter 
+        playerCounter = ++playerCounter 
+        parcialPlayerScore.textContent = playerCounter
+        lastGameResult.textContent = "Victoria"
+        if (playerCounter == 5 || computerCounter == 5) {verifyResult()} 
     } else {
-        return ++computerCounter
+        computerCounter = ++computerCounter
+        parcialComputerScore.textContent = computerCounter
+        lastGameResult.textContent = "Derrota"
+        if (playerCounter == 5 || computerCounter == 5) {verifyResult()} 
     }
 }
 
-function game(){
-    alert("Let's play best of 5")
-    for (let i = 0; i < 5; i++) {
-        playRound(getComputerChoice(), (prompt("Please, Choose one: Rock, Paper or Scissors")).toLowerCase())
-        console.log(playerCounter)
-        console.log(computerCounter)
-    }
-    if (playerCounter === computerCounter) {
-        alert("empate")
-    } else if (playerCounter > computerCounter) {
-        alert("ganaste")
-    } else if (playerCounter < computerCounter) {
-        alert("perdiste")
-    }
-}
 
-game()
 
 
 
